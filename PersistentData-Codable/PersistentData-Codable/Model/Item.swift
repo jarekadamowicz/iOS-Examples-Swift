5426373b8c710a7ccad6d4554f42e47651f06cd2
yrk <yarkill@gmail.com>
2020-09-07 12:26:39 +0200

    Added persistent storage example with Coding protocol

---
+++ PersistentData-Codable/PersistentData-Codable/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  PersistentData-Codable
+//
+//  Created by Jarek Adamowicz on 07/09/2020.
+//  Copyright © 2020 Jarek Adamowicz. All rights reserved.
+//
+
+import Foundation
+
+struct Item : Encodable, Decodable {
+    var name : String
+    var isChecked : Bool
+}
